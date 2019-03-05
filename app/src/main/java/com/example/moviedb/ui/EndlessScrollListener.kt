5f62d0fa93329c5ee194e46acebaaaa707039a34
nvquangth <nguyenvanquang96th@gmail.com>
2019-03-05 11:29:54 +0700

    Fix comment 2

--- app/src/main/java/com/example/moviedb/ui/EndlessScrollListener.kt
+++ app/src/main/java/com/example/moviedb/ui/EndlessScrollListener.kt
@@ -1,14 +1,28 @@
 package com.example.moviedb.ui
 
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
-abstract class EndlessScrollListener(val layoutManager: LinearLayoutManager) :
+abstract class EndlessScrollListener(private val layoutManager: RecyclerView.LayoutManager) :
     RecyclerView.OnScrollListener() {
     override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
         super.onScrolled(recyclerView, dx, dy)
-        if (layoutManager.findLastVisibleItemPosition() == layoutManager.itemCount - 1) {
-            loadMore()
+        if (layoutManager is LinearLayoutManager) {
+            if (layoutManager.findLastVisibleItemPosition() == layoutManager.itemCount - 1) {
+                loadMore()
+            }
+        } else if (layoutManager is GridLayoutManager) {
+            if (layoutManager.findLastVisibleItemPosition() == layoutManager.itemCount - 1) {
+                loadMore()
+            }
+        } else if (layoutManager is StaggeredGridLayoutManager) {
+            if (layoutManager.findLastVisibleItemPositions(IntArray(layoutManager.spanCount))[0] ==
+                layoutManager.itemCount - 1
+            ) {
+                loadMore()
+            }
         }
     }
 
