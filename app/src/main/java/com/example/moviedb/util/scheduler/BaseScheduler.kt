82b65010c05b2795f1746a7f15174f88f650b59e
nguyen.van.quangc <FRAMGIA\nguyen.van.quangc@b121544-pc.framgia.com>
2019-04-03 14:35:09 +0700

    Clean library

--- app/src/main/java/com/example/moviedb/util/scheduler/BaseScheduler.kt
+++ app/src/main/java/com/example/moviedb/util/scheduler/BaseScheduler.kt
@@ -1,13 +1,9 @@
 package com.example.moviedb.util.scheduler
-import io.reactivex.Scheduler
+
 import kotlinx.coroutines.CoroutineScope
 import kotlin.coroutines.CoroutineContext
 
 interface BaseScheduler {
-
-    fun io(): Scheduler
-
-    fun ui(): Scheduler
 
     val ioContext: CoroutineContext
 
