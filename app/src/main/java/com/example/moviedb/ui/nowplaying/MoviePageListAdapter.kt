8b878dd64c539de5ad85530ad84ea716e97fefde
nguyen.van.quangc <FRAMGIA\nguyen.van.quangc@b121544-pc.framgia.com>
2019-04-04 10:36:14 +0700

    Refactor paging to data binding

--- app/src/main/java/com/example/moviedb/ui/nowplaying/MoviePageListAdapter.kt
+++ app/src/main/java/com/example/moviedb/ui/nowplaying/MoviePageListAdapter.kt
@@ -1,68 +1,89 @@
 package com.example.moviedb.ui.nowplaying
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.paging.PagedListAdapter
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.RecyclerView
 import com.example.moviedb.R
+import com.example.moviedb.base.BasePageListAdapter
 import com.example.moviedb.data.model.Movie
-import com.example.moviedb.data.paging.NetworkState
-import java.lang.IllegalArgumentException
+import com.example.moviedb.data.paging.Status
+import com.example.moviedb.databinding.ItemMovieBinding
+import com.example.moviedb.databinding.NetworkStateItemBinding
 
-class MoviePageListAdapter(private val listener: (Movie) -> Unit, private val retryCallback: () -> Unit) :
-    PagedListAdapter<Movie, RecyclerView.ViewHolder>(object : DiffUtil.ItemCallback<Movie>() {
-        override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean =
-            oldItem == newItem
-
-        override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean =
-            oldItem.mId == newItem.mId
-    }) {
-
-    private var networkState: NetworkState? = null
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return when (viewType) {
-            R.layout.item_movie_2 -> MovieItemViewHolder.create(parent, listener)
-            R.layout.network_state_item -> NetworkStateItemViewHolder.create(parent, retryCallback)
-            else ->throw IllegalArgumentException("unknown view type $viewType")
-        }
+class MoviePageListAdapter(
+    private val listener: (Movie) -> Unit,
+    private val retryCallback: () -> Unit
+) : BasePageListAdapter<Movie>(object : DiffUtil.ItemCallback<Movie>() {
+    override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+        return oldItem.mId == newItem.mId
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        when(getItemViewType(position)) {
-            R.layout.item_movie_2 -> (holder as MovieItemViewHolder).bind(getItem(position))
-            R.layout.network_state_item -> (holder as NetworkStateItemViewHolder).binTo(networkState)
-        }
+    override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+        return oldItem == newItem
     }
+}) {
 
     override fun getItemViewType(position: Int): Int {
         return if (hasExtraRow() && position == itemCount - 1) {
             R.layout.network_state_item
         } else {
-            R.layout.item_movie_2
+            R.layout.item_movie
         }
     }
 
-    override fun getItemCount(): Int {
-        return super.getItemCount() + if (hasExtraRow()) 1 else 0
+    override fun createBinding(parent: ViewGroup, viewType: Int): ViewDataBinding {
+        when (viewType) {
+            R.layout.item_movie -> {
+                val binding: ViewDataBinding = DataBindingUtil.inflate(
+                    LayoutInflater.from(parent.context),
+                    R.layout.item_movie,
+                    parent,
+                    false
+                )
+                binding.root.setOnClickListener {
+                    if (binding is ItemMovieBinding) {
+                        binding.item?.let {
+                            listener.invoke(it)
+                        }
+                    }
+                }
+                return binding
+            }
+            R.layout.network_state_item -> {
+                val binding: ViewDataBinding = DataBindingUtil.inflate(
+                    LayoutInflater.from(parent.context),
+                    R.layout.network_state_item,
+                    parent,
+                    false
+                )
+                if (binding is NetworkStateItemBinding) {
+                    binding.retryButton.setOnClickListener {
+                        retryCallback.invoke()
+                    }
+                }
+                return binding
+            }
+            else -> throw IllegalArgumentException("unknown view type $viewType")
+        }
     }
 
-    private fun hasExtraRow() = networkState != null && networkState != NetworkState.LOADED
-
-    fun setNetworkState(newNetworkState: NetworkState?) {
-
-        val previousState = this.networkState
-        val hadExtraRow = hasExtraRow()
-        this.networkState = newNetworkState
-        val hasExtraRow = hasExtraRow()
-        if (hadExtraRow != hasExtraRow) {
-            if (hadExtraRow) {
-                notifyItemRemoved(super.getItemCount())
-            } else {
-                notifyItemInserted(super.getItemCount())
+    override fun bind(binding: ViewDataBinding, position: Int) {
+        when (getItemViewType(position)) {
+            R.layout.item_movie -> {
+                if (binding is ItemMovieBinding) {
+                    binding.item = getItem(position)
+                }
             }
-        } else if (hasExtraRow && previousState != newNetworkState) {
-            notifyItemChanged(itemCount - 1)
+            R.layout.network_state_item -> {
+                if (binding is NetworkStateItemBinding) {
+                    binding.isShowProgressBar = networkState?.status == Status.RUNNING
+                    binding.isRetry = networkState?.status == Status.FAILED
+                    binding.isShowMsg = networkState?.msg != null
+                    binding.errorMessage = networkState?.msg
+                }
+            }
         }
     }
 }
